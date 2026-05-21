4267bf653804225ae8673496b1eec0666b66d382
Leninpro17 <lpavelmeza@gmail.com>
2026-05-21 15:37:39 +0200

    Update and rename requirements.txt to send_message.py

---
+++ send_message.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = "✅ Bot Sorare attivo"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
