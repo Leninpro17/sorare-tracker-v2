5fd57dc006fc2eecc393a9fb9a4017d8ef1529da
Leninpro17 <lpavelmeza@gmail.com>
2026-05-22 16:03:59 +0200

    Update build_proleague.py

--- build_proleague.py
+++ build_proleague.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 
 API_KEY = os.environ["API_FOOTBALL_KEY"]
 
@@ -8,9 +9,18 @@ headers = {
 }
 
 response = requests.get(
-    "https://v3.football.api-sports.io/leagues?search=Belgium",
+    "https://v3.football.api-sports.io/teams?league=144&season=2025",
     headers=headers
 )
 
 print(response.status_code)
-print(response.text)
+
+data = response.json()
+
+print("Teams found:", len(data["response"]))
+
+for team in data["response"]:
+    print(
+        team["team"]["id"],
+        team["team"]["name"]
+    )
