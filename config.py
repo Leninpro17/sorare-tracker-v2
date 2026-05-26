709af8788dced29da1d43c39570d646813f555ab
Leninpro17 <lpavelmeza@gmail.com>
2026-05-26 10:58:06 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# config.py
+
+LEAGUES = {
+    "belgium": {
+        "name": "Jupiler Pro League",
+        "api_league_id": 144,
+        "country": "Belgium"
+    },
+    "croatia": {
+        "name": "HNL",
+        "api_league_id": 210,
+        "country": "Croatia"
+    },
+    "segunda": {
+        "name": "Segunda División",
+        "api_league_id": 141,
+        "country": "Spain"
+    },
+    "bundesliga2": {
+        "name": "2. Bundesliga",
+        "api_league_id": 79,
+        "country": "Germany"
+    }
+}
